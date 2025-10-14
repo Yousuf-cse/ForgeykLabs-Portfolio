0ea4dbb9ad98db82093e49acedf753f16a84a4f0
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-10-14 21:14:17 +0530

    feat: add scroll animation in call-to-action.tsx

--- components/landing-page/call-to-action.tsx
+++ components/landing-page/call-to-action.tsx
@@ -1,28 +1,38 @@
-import Image from "next/image"
+"use client"
+
+import { useRef } from "react"
 import ContactFormButton from "./contact-form-button"
+import { motion, useInView } from "framer-motion";
+import { staggerContainer, fadeInUp, scaleIn } from "@/lib/animations";
 
 export default function CallToAction() {
+  const sectionRef = useRef(null);
+    const isInView = useInView(sectionRef, {
+      once: false,
+      amount: 0.2,
+    });
+
   return (
-    <section id="contact" className="my-20 flex justify-center">
-      <div className="relative overflow-hidden max-w-6xl w-full">
+    <section id="contact" className="my-20 flex justify-center" ref={sectionRef}>
+      <motion.div variants={staggerContainer} initial="hidden" animate={isInView ? "visible" : "hidden"} className="relative overflow-hidden max-w-6xl w-full">
         <div className="p-8 md:p-10 lg:p-12 flex flex-col items-center text-center">
           {/* Text content - centered */}
           <div className="z-10 max-w-2xl">
-            <h2 className="text-black dark:text-white mb-6 text-3xl md:text-4xl lg:text-5xl font-medium leading-tight">
+            <motion.h2 variants={fadeInUp} className="text-black dark:text-white mb-6 text-3xl md:text-4xl lg:text-5xl font-medium leading-tight">
               Your Next Digital Move <span className="text-[#7A7FEE] dark:text-[#7A7FEE]">Starts</span> Here
-            </h2>
-            <p className="my-6 text-sm md:text-base text-gray-700 dark:text-gray-300">
+            </motion.h2>
+            <motion.p variants={fadeInUp} className="my-6 text-sm md:text-base text-gray-700 dark:text-gray-300">
               Have an idea or a problem that needs solving?
-            </p>
-            <p className="mb-6 text-sm md:text-base text-gray-700 dark:text-gray-300">
+            </motion.p>
+            <motion.p variants={fadeInUp} className="mb-6 text-sm md:text-base text-gray-700 dark:text-gray-300">
               Let's chat. We'll help you explore the best approach, map out a plan, and see if we're the right fit.
-            </p>
-            <div className="flex justify-center">
+            </motion.p>
+            <motion.div variants={fadeInUp} className="flex justify-center">
               <ContactFormButton />
-            </div>
+            </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
